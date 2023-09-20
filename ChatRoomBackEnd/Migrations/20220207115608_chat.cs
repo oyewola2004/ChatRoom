c4d9517df3ff37f930172e4bdea506d4afd49ec0
oyewola2004 <102753400+oyewola2004@users.noreply.github.com>
2023-09-20 08:02:26 -0700

    first commit

---
+++ ChatRoomBackEnd/Migrations/20220207115608_chat.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ChatRoomBackEnd.Migrations
+{
+    public partial class chat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
